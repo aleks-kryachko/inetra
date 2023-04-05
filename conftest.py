a9e65deded9774d6c0139787cfd93f166b65a08b
aleksandr kryachko <aleksan.kryachko@gmail.com>
2023-04-05 21:45:41 +0700

    web_conftest

--- conftest.py
+++ conftest.py
@@ -1,6 +1,23 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
 url_ping = 'http://195.189.239.54'
 url_json1 = 'http://jsonplaceholder.typicode.com/todos'
 url_selenium ='http://www.python.org'
 url_json2 = 'https://petstore.swagger.io/v2/pet'
 host_sent =''
 host_web ="http://195.189.239.54/"
+
+@pytest.fixture
+def browser():
+    browser = webdriver.Chrome(executable_path=".chromedriver.exe")
+    # browser = webdriver.Chrome()
+    browser.set_window_size(1416, 1026)
+    # browser.maximize_window()
+    url = host_web
+    # print(browser.get_window_size())
+    browser.get(url=url)
+    browser.set_page_load_timeout(10) # sets timeout to 10 sec
+    yield browser
+    browser.quit()
--- tests/test_web_channels.py
+++ tests/test_web_channels.py
@@ -7,22 +7,22 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from conftest import host_web
-
+from conftest import browser
 
 url = host_web
 
-@pytest.fixture
-def browser():
-    browser = webdriver.Chrome(executable_path=".chromedriver.exe")
-    # browser = webdriver.Chrome()
-    browser.set_window_size(1416, 1026)
-    # browser.maximize_window()
-    url = host_web
-    # print(browser.get_window_size())
-    browser.get(url=url)
-    browser.set_page_load_timeout(10) # sets timeout to 10 sec
-    yield browser
-    browser.quit()
+# @pytest.fixture
+# def browser():
+#     browser = webdriver.Chrome(executable_path=".chromedriver.exe")
+#     # browser = webdriver.Chrome()
+#     browser.set_window_size(1416, 1026)
+#     # browser.maximize_window()
+#     url = host_web
+#     # print(browser.get_window_size())
+#     browser.get(url=url)
+#     browser.set_page_load_timeout(10) # sets timeout to 10 sec
+#     yield browser
+#     browser.quit()
 def test_01_main_page(browser):
     browser.find_element(By.LINK_TEXT, 'Журналы').click()
     assert browser.find_element(By.CLASS_NAME, 'button'), 'button'
