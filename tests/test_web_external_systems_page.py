2e07451c5488deb50b1f7dbd145dc93a7e6ee453
aleksandr kryachko <aleksan.kryachko@gmail.com>
2023-04-09 11:30:35 +0700

    страница внешнии системы

--- tests/test_web_external_systems_page.py
+++ tests/test_web_external_systems_page.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import pytest
 import time
 from selenium.webdriver.common.keys import Keys
@@ -21,10 +22,12 @@ def test_01_templates_logs_items(browser):
     assert browser.find_element(By.LINK_TEXT, 'Наименование'), 'поле Наименование'
     assert browser.find_element(By.LINK_TEXT, 'Приём заданий на рассылку'), 'поле Прием задания на рассылку'
     assert browser.find_element(By.LINK_TEXT, 'Приоритет'), 'поле Приоритет'
-def test_02():
-    assert browser.find_element(By.CLASS_NAME, 'button').click()
+    browser.find_element(By.CLASS_NAME, 'button').click()
     assert browser.find_element(By.CLASS_NAME, 'header__main'), 'Шапка'
     assert browser.find_element(By.CLASS_NAME, 'button-back'), 'вернуться'
-    assert browser.find_element(By.LINK_TEXT, 'Код'), 'Код'
-    assert browser.find_element(By.LINK_TEXT, 'ID'), 'ID'
-    assert browser.find_element(By.CLASS_NAME, 'button button-clear'), 'Очистить'
+    assert browser.find_element(By.ID, 'code'), 'Код'
+    assert browser.find_element(By.ID, 'name'), 'Наименованние'
+    assert browser.find_element(By.ID, 'priority'), 'Приоритет'
+    assert browser.find_element(By.ID, 'is_mailing'), 'Разрешен прием заданий'
+    assert browser.find_element(By.ID, 'events'), 'Событие'
+    assert browser.find_element(By.CLASS_NAME, 'buttons-bottom'), 'Сохранить'
