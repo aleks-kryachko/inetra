c43391c9c01cc2fd2a950e0edf60f23769513d87
aleksandr kryachko <aleksan.kryachko@gmail.com>
2023-04-09 17:58:10 +0700

    страница каналы оптимизация

--- tests/test_web_channels.py
+++ tests/test_web_channels.py
@@ -26,21 +26,37 @@ from conftest import browser
 def test_01_main_page(browser):
     browser.find_element(By.LINK_TEXT, 'Каналы').click()
     assert browser.find_element(By.CLASS_NAME, 'button'), 'Элемент button'
-    assert browser.find_element(By.CLASS_NAME, 'main'), 'Таблица'
+
+    def test_02():
+        assert browser.find_element(By.CLASS_NAME, 'main'), 'Таблица'
     # assert browser.find_element(By.CLASS_NAME, 'column desc'), 'Элемент ID'
-    assert browser.find_element(By.CLASS_NAME, 'table-filter'), 'Фильтр'
+    def test_03():
+        assert browser.find_element(By.CLASS_NAME, 'table-filter'), 'Фильтр'
     # assert browser.find_element(By.LINK_TEXT, 'Тип канала'), 'Элемент Тип канала'
     # assert browser.find_element(By.LINK_TEXT, 'Отправитель'), 'Элемент Отправитель'
     # assert browser.find_element(By.LINK_TEXT, 'По умолчанию'), 'Элемент По умолчанию'
     # assert browser.find_element(By.LINK_TEXT, 'Количество попыток'), 'Элемент Количество попыток'
     # assert browser.find_element(By.LINK_TEXT, 'Таймаут'), 'Элемент Таймаут'
     # assert browser.find_element(By.LINK_TEXT, 'Email SMTP'), 'Элемент Email SMTP'
-    browser.find_element(By.CLASS_NAME, 'button').click()
-    assert browser.find_element(By.CLASS_NAME, 'button-back'), 'Элемент Вернуться'
-    assert browser.find_element(By.CLASS_NAME, 'main'), 'Элемент Таблица'
-    assert browser.find_element(By.ID, 'channel_type'), 'Элемент Наименование'
-    assert browser.find_element(By.ID, 'send_count'), 'Элемент Количество попыток'
-    assert browser.find_element(By.ID, 'is_default'), 'По умолчанию'
-    assert browser.find_element(By.CLASS_NAME, 'form-input-clear'), 'Элемент Очистить строку'
-    assert browser.find_element(By.CLASS_NAME, 'buttons-bottom'), 'Элемент Кнопки'
+    def test_04():
+        browser.find_element(By.CLASS_NAME, 'button').click()
+        assert browser.find_element(By.CLASS_NAME, 'button-back'), 'Элемент Вернуться'
+
+    def test_05():
+        assert browser.find_element(By.CLASS_NAME, 'main'), 'Элемент Таблица'
+
+    def test_06():
+        assert browser.find_element(By.ID, 'channel_type'), 'Элемент Наименование'
+
+    def test_07():
+        assert browser.find_element(By.ID, 'send_count'), 'Элемент Количество попыток'
+
+    def test_08():
+        assert browser.find_element(By.ID, 'is_default'), 'По умолчанию'
+
+    def test_09():
+        assert browser.find_element(By.CLASS_NAME, 'form-input-clear'), 'Элемент Очистить строку'
+
+    def test_10():
+        assert browser.find_element(By.CLASS_NAME, 'buttons-bottom'), 'Элемент Кнопки'
 
